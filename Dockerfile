db39efef6806f8321b62ce3a54cdf2b35f3436e6
an958266 <andrea.nuzzo@ufl.edu>
2020-09-11 13:29:33 +0100

    andreanuzzo/mmim:0.2

--- Dockerfile
+++ Dockerfile
@@ -2,22 +2,25 @@
 FROM rocker/rstudio:3.6.2
 
 # Debian libraries
+
 ARG DEBIAN_FRONTEND=noninteractive
+
 RUN apt-get update
+
 RUN apt-get install -y python3.6 python3-pip python3-venv libxml2-dev r-cran-devtools
 
-## R packages
-ARG RENV_VERSION=0.12.0 
-RUN  R -e "install.packages('remotes', repos = c(CRAN = 'https://cloud.r-project.org'))" && \
-  R -e "remotes::install_github('rstudio/renv@${RENV_VERSION}')" && \
-  R -e 'renv::restore()'
+## packages
 
-## Python environment (requires venv)
-COPY requirements.txt /tmp/requirements.txt
-RUN /bin/bash -c "python3 -m venv .venv && \
-  source .venv/bin/activate && \
-  yes | pip3 install wheel numpy==1.18.1 matplotlib==3.1.1 pandas==0.25.3 && \
-  yes | pip3 install -r /tmp/requirements.txt && \
-  deactivate"
-COPY .venv 
-  
+COPY --chown=rstudio:rstudio renv.lock /home/rstudio/renv.lock
+COPY --chown=rstudio:rstudio requirements.txt /home/rstudio/requirements.txt
+
+ENV RENV_VERSION=0.12.0 
+
+RUN R -e "install.packages('remotes', repos = c(CRAN = 'https://cloud.r-project.org'))"
+RUN R -e "remotes::install_github('rstudio/renv@${RENV_VERSION}')"
+RUN R -e "renv::restore('/home/rstudio/')"
+
+
+RUN chmod 700 /home/rstudio/MMIM/scripts/get_data.sh
+
+CMD bash /home/rstudio/MMIM/scripts/get_data.sh /home/rstudio/MMIM
--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 basepath=$(echo "$1/$line")
 
 cd $basepath
@@ -7,20 +7,20 @@ mkdir raw_data
 mkdir lookup_tables
 
 #Sample data
-wget https://ibdmdb.org/tunnel/products/HMP2/Metadata/hmp2_metadata.csv -O raw_data/hmp2_metadata.csv
+wget --no-check-certificate https://ibdmdb.org/tunnel/products/HMP2/Metadata/hmp2_metadata.csv -O raw_data/hmp2_metadata.csv 
 
 #Metabolomics tables
-wget https://ibdmdb.org/tunnel/products/HMP2/Metabolites/1723/HMP2_metabolomics.csv.gz -O raw_data/HMP2_metabolomics.csv.gz
+wget --no-check-certificate https://ibdmdb.org/tunnel/products/HMP2/Metabolites/1723/HMP2_metabolomics.csv.gz -O raw_data/HMP2_metabolomics.csv.gz
 
 #Host transcriptomes
-wget https://ibdmdb.org/tunnel/products/HMP2/HTX/1730/host_tx_counts.tsv.gz -O raw_data/host_tx_counts.tsv.gz
+wget --no-check-certificate https://ibdmdb.org/tunnel/products/HMP2/HTX/1730/host_tx_counts.tsv.gz -O raw_data/host_tx_counts.tsv.gz
 
 #HMDB database
-wget http://www.hmdb.ca/system/downloads/current/hmdb_metabolites.zip -O raw_data/HMDB.zip
+wget --no-check-certificate http://www.hmdb.ca/system/downloads/current/hmdb_metabolites.zip -O raw_data/HMDB.zip
 unzip raw_data/HMDB.zip -d raw_data
 
 source .venv/bin/activate && python3 scripts/HMDB_parser.py && deactivate
 
 #Supplementary Tables from Lloyd-Price et al. 2020
-wget -q -O raw_data/.tmp.zip https://static-content.springer.com/esm/art%3A10.1038%2Fs41586-019-1237-9/MediaObjects/41586_2019_1237_MOESM6_ESM.zip 
+wget --no-check-certificate -q -O raw_data/.tmp.zip https://static-content.springer.com/esm/art%3A10.1038%2Fs41586-019-1237-9/MediaObjects/41586_2019_1237_MOESM6_ESM.zip 
 unzip raw_data/.tmp.zip -d elaborated_data
